f1567492c4d1ca2ab211b215f177296acec6f98b
hanweiwu <hanwei_wu@163.com>
2024-02-20 10:11:50 +0800

    PY32F002A 添加机器人红外遥控键值码

---
+++ PY32F002A/程序代码/PY32F002A/Bsp/bsp_ir.h
@@ -0,0 +1,44 @@
+#ifndef __BSP_IR__
+#define __BSP_IR__
+
+#include "common.h"
+
+#define UP					0x01FE
+#define DOWN			0x02FD
+#define LEFT				0x06F9
+#define RIGHT			0x07F8
+#define OK					0x05FA
+#define BLEFT			0x03FC
+#define BRIGHT		0x04FB
+
+#define LIGHT_ENHANCE_1		UP
+#define LIGHT_WEAKEN_1		DOWN
+#define LIGHT_TOOGLE_1		OK
+#define LIGHT_TOOGLE_2		BRIGHT
+
+
+typedef struct _IR_STR {
+	uint8_t rxDr;//1:一帧数据接收完毕 0:不解析
+	uint8_t startFlag;//1:解码开始  0:非解码
+	uint8_t count;//280us计数次数
+	uint8_t irData[4];//用户码2个字节+键值码2个字节
+	uint8_t irBitData[32];//4字节32位计时数组
+} IR_STR;
+
+typedef struct _LED_STR{
+	int8_t r_pulse;
+	int8_t g_pulse;
+	int8_t b_pulse;
+} LED_STR;
+
+extern LED_STR led_str;
+extern IR_STR ir_str;
+
+
+int8_t ri_data_parse( void );
+void execute_led( void );
+
+
+#endif /*__BSP_IR__*/
+
+
---
+++ PY32F002A/程序代码/PY32F002A/Bsp/common.h
@@ -0,0 +1,22 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+/* LIB ------------------------------------------------------------*/
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+/* Inc ------------------------------------------------------------*/
+#include "main.h"
+#include "usart.h"
+#include "gpio.h"
+#include "tim.h"
+
+/* BSP ------------------------------------------------------------*/
+#include "bsp_ir.h"
+
+
+/* Tools ------------------------------------------------------------*/
+
+#endif /* __COMMON_H__ */
